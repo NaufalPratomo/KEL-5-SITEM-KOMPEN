9fdc8d5a8bc21c1fbb71878625649bf1a92d6376
Naufal Pratomo <naufalpratomo8@gmail.com>
2023-09-15 09:25:17 +0700

    first commit

---
+++ sistem_kompen.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class sistem_kompen {
+       public static void main (String [] hady){
+
+        String nama , nim ;
+
+        final double kompen_s1 , kompen_s2, poin_akhir;
+
+        try (Scanner input = new Scanner (System.in)) {
+            System.out.println("===== Menghitung Nilai Kompen Mahasiswa=====\n");
+            
+                    System.out.print("Masukan nama : ");
+            
+                    nama = input.nextLine();
+            
+                    System.out.print("Masukan NIM : ");
+            
+                    nim = input.nextLine();
+            
+                    System.out.print("Kompen Semester 1 : ");
+            
+                    kompen_s1 = input.nextInt();
+            
+                    System.out.print("Kompen semester 2 : ");
+            
+                    kompen_s2 = input.nextInt();
+            
+                  
+        }
+
+  poin_akhir = (2 * kompen_s1 + 2 * kompen_s2);
+
+        System.out.println(nama+" dengan NIM "+nim+" memiliki nilai kompen akhir "+poin_akhir);
+
+    }
+
+}
