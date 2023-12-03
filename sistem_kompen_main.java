ae8f0353b7c0fe1a677b9715b45701888cc9ded7
dhanisaptrri <143993941+dhanisaptrri@users.noreply.github.com>
2023-12-03 15:16:06 +0700

    memperbaiki sistem penghitungan kompen

--- sistem_kompen_main.java
+++ sistem_kompen_main.java
@@ -1,71 +1,150 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class sistem_kompen_main {
+    static Scanner input = new Scanner(System.in);
 
-    // Clear the screen
+    // Deklarasi
+    static String nimMhs;
+    static String passMhs;
+    static String nama, kelas;
+    static int semester;
+    static double poin_akhir = 0;
+    static int baris = 5, kolom = 9, kerja = 0;
+
+    // Fungsi Clear the screen
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
-        System.out.flush(); 
+        System.out.flush();
+    }
+
+    // Fungsi pilih semester
+    public static int pilihSemester() {
+        // Input semester (pilih semester)
+        System.out.println("╔════════════════════════════════════════════╗");
+        System.out.println("║       Perhitungan Kompen Mahasiswa         ║");
+        System.out.println("╚════════════════════════════════════════════╝");
+        System.out.print("Masukkan Semester (1-8): ");
+        semester = input.nextInt();
+        if (semester > 8 || semester < 1) {
+            System.out.println("Semester tidak valid.");
+            pilihSemester();
+        }
+        return semester;
+    }
+
+    // Fungsi konfirmasi Semester
+    public static int konfirmasiSemester(int kompen) {
+        // Rekursif
+        System.out.print("Apakah Kompen sudah dikerjakan (y/t) : ");
+        String jawabKonfirmasi = input.next().toLowerCase();
+        switch (jawabKonfirmasi) {
+            case "y":
+                kompen *= 0;
+                break;
+            case "t":
+                kompen *= 2;
+                break;
+            default:
+                System.out.println("Pilihan anda salah");
+                konfirmasiSemester(kompen);
+                break;
+        }
+        return kompen;
+    }
+
+    //Fungsi Headline menu Admin
+    public static void hlAdmin (){
+            System.out.println("╔════════════════════════════════════════════╗");
+            System.out.println("           Selamat datang Admin               ");
+            System.out.println("║          Silahkan Pilih Menu :             ║");
+            System.out.println("╚════════════════════════════════════════════╝");
+    }
+
+    //Fungsi Headline menu Mahasiswa
+    public static void hlMhs (){
+            System.out.println("╔════════════════════════════════════════════╗");
+            System.out.println("           Selamat datang " + nama + "            ");
+            System.out.println("║          Silahkan Pilih Menu :             ║");
+            System.out.println("╚════════════════════════════════════════════╝");
     }
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-    
-    System.out.println("█▀ █▀▀ █░░ ▄▀█ █▀▄▀█ ▄▀█ ▀█▀  █▀▄ ▄▀█ ▀█▀ ▄▀█ █▄░█ █▀▀  █▀▄ █  █▀ █ █▀ ▀█▀ █▀▀ █▀▄▀█  █▄▀ █▀█ █▀▄▀█ █▀█ █▀▀ █▄░█ ");
-    System.out.println("▄█ ██▄ █▄▄ █▀█ █░▀░█ █▀█ ░█░  █▄▀ █▀█ ░█░ █▀█ █░▀█ █▄█  █▄▀ █  ▄█ █ ▄█ ░█░ ██▄ █░▀░█  █░█ █▄█ █░▀░█ █▀▀ ██▄ █░▀█ ");
+        System.out.println(
+                "█▀ █▀▀ █░░ ▄▀█ █▀▄▀█ ▄▀█ ▀█▀  █▀▄ ▄▀█ ▀█▀ ▄▀█ █▄░█ █▀▀  █▀▄ █  █▀ █ █▀ ▀█▀ █▀▀ █▀▄▀█  █▄▀ █▀█ █▀▄▀█ █▀█ █▀▀ █▄░█ ");
+        System.out.println(
+                "▄█ ██▄ █▄▄ █▀█ █░▀░█ █▀█ ░█░  █▄▀ █▀█ ░█░ █▀█ █░▀█ █▄█  █▄▀ █  ▄█ █ ▄█ ░█░ ██▄ █░▀░█  █░█ █▄█ █░▀░█ █▀▀ ██▄ █░▀█ ");
 
-        // Deklarasi
-        String nimMhs;
-        String passMhs;
-        String nama, kelas;
-        int semester;
-        double poin_akhir = 0;
-        int baris=5,kolom=9;
-        
         // Array 2D List Kompen Setiap Mahasiswa Pada Kelas yang Diinputkan
-    String[][] kompenMhs = {
-            { "Nama", "S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8" },
-            { "Naufal", "2", "0", "1" },
-            { "Faishal", "2", "1", "3" },
-            { "Dhanisa", "2", "2", "3" },
-            { "Valen", "2", "1", "0" },
+        String[][] kompenMhs = {
+                { "Nama", "S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8" },
+                { "Naufal", "2", "0", "1" },
+                { "Faishal", "2", "1", "3" },
+                { "Dhanisa", "2", "2", "3" },
+                { "Valen", "2", "1", "0" },
         };
 
-        // Array for login data (NIM and password)
-    String[] loginData = {
+        // Array untuk login data (NIM and password) for Admin
+        String[] loginDataAdmin = { 
+            "admin", "adminpass", 
+            "1", "1" 
+        };
+
+        // Array untuk login data (NIM and password) for Mahasiswa
+        String[] loginDataMhs = { 
             "2341720075", "1", 
             "2341720212", "1", 
             "2341720218", "1", 
-            "2341720016", "1",
-            "1", "1"
+            "2341720016", "1" 
         };
 
-        // Decoration
+        // Dekorasi
         System.out.println("╔════════════════════════════════════════════╗");
         System.out.println("║          Sistem Kompen Mahasiswa           ║");
         System.out.println("╚════════════════════════════════════════════╝");
 
-        //login
+        // login
         System.out.println("===== Sistem Kompen Mahasiswa =====\n");
         boolean loginSukses = false;
+        int pilihan = 0;
         while (!loginSukses) {
+            System.out.print("Login sebagai (1. Admin, 2. Mahasiswa): ");
+            pilihan = input.nextInt();
+            input.nextLine(); // Mengonsumsi newline
+
             System.out.print("Masukkan NIM: ");
             nimMhs = input.nextLine();
             System.out.print("Masukkan Password: ");
             passMhs = input.nextLine();
 
-            boolean found = false;
-            for (int i = 0; i < loginData.length; i += 2) {
-                if (nimMhs.equals(loginData[i]) && passMhs.equals(loginData[i + 1])) {
-                    found = true;
-                    loginSukses = true;
+            switch (pilihan) {
+                case 1:
+                    // Cek login admin
+                    for (int i = 0; i < loginDataAdmin.length; i += 2) {
+                        if (nimMhs.equals(loginDataAdmin[i]) && passMhs.equals(loginDataAdmin[i + 1])) {
+                            loginSukses = true;
+                            nama = "Admin";
+                            break;
+                        }
+                    }
                     break;
-                }
+
+                case 2:
+                    // Cek login mahasiswa
+                    for (int i = 0; i < loginDataMhs.length; i += 2) {
+                        if (nimMhs.equals(loginDataMhs[i]) && passMhs.equals(loginDataMhs[i + 1])) {
+                            loginSukses = true;
+                            break;
+                        }
+                    }
+                    break;
+
+                default:
+                    System.out.println("Pilihan anda salah");
+                    break;
             }
 
-            if (!found) {
+            if (!loginSukses) {
                 System.out.println("NIM atau Password salah. Coba lagi.");
             }
         }
@@ -83,73 +162,85 @@ public class sistem_kompen_main {
         kelas = input.nextLine();
         clearScreen(); // Clear the screen
 
-        //Fungsi untuk memasukkan input kompen kedalam array 2D list kompen 
-    String[][] newMhs = new String[baris+1][kolom];
+        // Fungsi untuk memasukkan input kompen kedalam array 2D list kompen
+        String[][] newMhs = new String[baris + 1][kolom];
         for (int i = 0; i < kompenMhs.length; i++) {
             for (int j = 0; j < kompenMhs[i].length; j++) {
-                newMhs[i][j]=kompenMhs[i][j];
+                newMhs[i][j] = kompenMhs[i][j];
             }
         }
-        kompenMhs=newMhs;
-        kompenMhs[baris][0]=nama;
-        
+        kompenMhs = newMhs;
+        kompenMhs[baris][0] = nama;
+
         // Menu Sistem Kompen
         boolean sistemKompen = true;
         int menu;
         do {
-            System.out.println("╔════════════════════════════════════════════╗");
-            System.out.println("           Selamat datang "+ nama +"            ");
-            System.out.println("║          Silahkan Pilih Menu :             ║");
-            System.out.println("╚════════════════════════════════════════════╝");
-            System.out.println("\nSelamat datang " + nama + ", Silahkan pilih menu: ");
+            if (pilihan==1) {
+                hlAdmin();
+            } else {
+                hlMhs();
+            }
             System.out.println("0. Keluar Program");
-            System.out.println("1. Perhitungan Kompen Mahasiswa");
-            System.out.println("2. List Kompen Pada Kelas " + kelas);
+
+            // Perubahan pada bagian ini
+            if (pilihan == 2) {
+                System.out.println("1. List Kompen Pada Kelas " + kelas);
+            } else if (pilihan == 1) {
+                System.out.println("1. Perhitungan Kompen Mahasiswa");
+                System.out.println("2. List Kompen Pada Kelas " + kelas);
+            }
+
             System.out.print("\nPilih menu: ");
             menu = input.nextInt();
             input.nextLine();
             clearScreen(); // Clear the screen
+
             switch (menu) {
                 case 0:
                     sistemKompen = false;
                     break;
                 case 1:
-                    // Input semester (pilih semester)
-                    System.out.println("╔════════════════════════════════════════════╗");
-                    System.out.println("║       Perhitungan Kompen Mahasiswa         ║");
-                    System.out.println("╚════════════════════════════════════════════╝");
-                    System.out.print("Perhitungan Kompen Mahasiswa");
-                    System.out.print("Masukkan Semester (1-8): ");
-                    semester = input.nextInt();
-
-                    // Input kompen sesuai semester
-                    int kompenSemester = 0;
-                    for (int i = 1; i <= semester; i++) {
-                        System.out.println("Anda memilih semester " + i + ".");
-                        System.out.print("Masukkan Kompen Semester " + i + ": ");
-                        int kompen = input.nextInt();
-                        String kompenString = Integer.toString(kompen);
-                        kompenMhs[baris][i]=kompenString;
-                        int kerja=kompen*2;
-                        kompenSemester += kerja;
-                    }
-
-                    poin_akhir = kompenSemester;
-
-                    // Hasil akhir
-                    System.out.println(nama + " kelas " + kelas + " semester " + semester
-                            + " memiliki nilai kompen akhir " + poin_akhir);
-                case 2:
-                    // Cetak array 2D list kompen
-                    for (int i = 0; i < kompenMhs.length; i++) {
-                        for (int j = 0; j < kompenMhs[i].length; j++) {
-                            if (kompenMhs[i][j]==null) {
-                                kompenMhs[i][j]="";
+                    // Memanggil fungsi pilihSemester
+                    if (pilihan == 1) {
+                        int simpanSemester = pilihSemester();
+                        // Input kompen sesuai semester
+                        int kompenSemester = 0;
+                        for (int i = 1; i <= simpanSemester; i++) {
+                            System.out.println("Anda memilih semester " + i + ".");
+                            System.out.print("Masukkan Kompen Semester (jam) " + i + ": ");
+                            int kompen = input.nextInt();
+                            input.nextLine();
+                            // memanggil fungsi untuk menanyakan apakah kompen sudah dikerjakan
+                            int simpanKompen = konfirmasiSemester(kompen);
+                            String kompenString = Integer.toString(simpanKompen);
+                            kompenMhs[baris][i] = kompenString;
+                            for (int j = 0; j < semester - 1; j++) {
+                                simpanKompen *= 2;
                             }
-                            System.out.print(kompenMhs[i][j] + "\t");
+                            kerja = simpanKompen;
+                            kompenSemester += kerja;
                         }
-                        System.out.println();
+                        poin_akhir = kompenSemester;
+
+                        // Hasil akhir
+                        System.out.println(nama + " kelas " + kelas + " semester " + semester
+                                + " memiliki nilai kompen akhir " + poin_akhir);
+                    } else {
+                        // Cetak array 2D list kompen
+                        for (int i = 0; i < kompenMhs.length; i++) {
+                            for (int j = 0; j < kompenMhs[i].length; j++) {
+                                if (kompenMhs[i][j] == null) {
+                                    kompenMhs[i][j] = "";
+                                }
+                                System.out.print(kompenMhs[i][j] + "\t");
+                            }
+                            System.out.println();
+                        }
                     }
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid");
             }
 
         } while (sistemKompen);
